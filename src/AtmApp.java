3f5a0cf6758e8a40b9e6730e2cded8273a4981fe
enver <mert.enver@gmail.com>
2023-04-28 15:01:45 +0300

    v4

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -7,6 +7,7 @@ public class AtmApp {
         //version2
         System.out.println("Learn Java earn money!!");
         //version3
-
+        System.out.println("Javayi ogrenmek cok kolay :)");
+        //version4
     }
 }
